fda1c65d24a6459f7b0ecaad50152a1cd44359aa
josue <josue@LAPTOP-QTPCCLL1>
2023-09-19 10:16:18 +0200

    commit inicial

---
+++ Tips_shortcuts.py
@@ -0,0 +1,34 @@
+## ubuntu
+'''cd = means change directory
+cd .. = one step back
+ls = list = the items that you have on that carpet
+cd mnt = change of directory mounting = it will take you to the files
+cd c = local user
+cd / = take you all the way back to the user or main current computer
+ the comandas go like this
+ cd / 
+ cd mnt
+ cd c
+ cd user
+ cd "your name"
+ ls
+ choose de file that you need to get in
+ cd "file"
+ ls
+ 
+ commands to clear
+  cd clear
+  
+  commands to create a new directory = folder
+  mkdir== making a directory
+  go like this will create a new directory and sub directory
+  mkdir test =  creating the new directory
+  ls = checking everything was executed correctly
+  mkdir service = ceting the sub directory
+  ls = check once again,
+  
+  commands to erase files
+  in order to erase or deleted those documents all we got to do is to delete the main one
+  rm -rf test ==  this way both files a
+  
+  '''
